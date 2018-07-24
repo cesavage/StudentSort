a526978117ad5a518f97457a26c14adfe10f5652
Chris Savage <cesavage@bsu.edu>
2018-07-23 22:31:53 -0400

    Create studentListItem layout.

--- app/src/main/java/bsu/edu/studentsort/JsonParser.java
+++ app/src/main/java/bsu/edu/studentsort/JsonParser.java
@@ -1,21 +1,15 @@
 package bsu.edu.studentsort;
 
 import android.content.Context;
-import android.util.Log;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class JsonParser{
     private Context mCurrentContext;
-    StudentStore mStudentStore = StudentStore.getStudentStore();
+    private StudentStore mStudentStore = StudentStore.getStudentStore();
 
 
     public JsonParser(Context currentContext){
@@ -48,6 +42,5 @@ public class JsonParser{
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
     }
 }
